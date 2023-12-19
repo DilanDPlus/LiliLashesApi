f74d0547989602d85dea44586317dba3f129a2ef
DilanDPlus <dilandplus@gmail.com>
2023-12-18 22:56:19 -0500

    Base de datos y puerto actualizado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 38470;
 
 const server = http.createServer(app);
 
